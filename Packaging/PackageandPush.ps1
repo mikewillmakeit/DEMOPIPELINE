78f2a021df951fee44b73a465d10f840bbffc3a3
mikewillmakeit <mikenatsfan@gmail.com>
2019-07-18 15:10:51 -0400

    fixed ip address block of chocolatey server

--- Packaging/PackageandPush.ps1
+++ Packaging/PackageandPush.ps1
@@ -1,4 +1,4 @@
 $Packagepath = 
 #Update path to where packages will be stored
 $files = Get-ChildItem -Path $Packagepath -Exclude *.Tests.ps1
-foreach ($file in $files) {choco push [$file] --source http://13.59.44.78/chocolatey}
+foreach ($file in $files) {choco push [$file] --source="http://13.59.44.78/chocolatey"}
